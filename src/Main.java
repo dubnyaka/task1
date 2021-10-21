79bd4edb19551d10f743a5e7d55624a9db6a32ce
dubnyaka <dubnyaka@gmail.com>
2021-10-21 13:52:39 +0300

    Top5Hashtags is done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Arrays;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static ArrayList<Integer> arrSort(ArrayList<Integer> arr) {
-        ArrayList<Integer> outputArr = new ArrayList<Integer>();
-        for (int element : arr) {
-            outputArr.add(element);
-        }
+        ArrayList<Integer> outputArr = new ArrayList<>(arr);
         // Removing negative elements
         outputArr.removeIf(n -> n < 0);
 
@@ -17,6 +14,41 @@ public class Main {
         return outputArr;
     }
 
+    public static List<Map.Entry<String, Integer>> topFiveHashtags(ArrayList<String> strings) {
+        HashMap<String, Integer> hashtagsCounter = new HashMap<>();
+
+        for (String string : strings) {
+            String pattern = "(#\\w*)";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(string);
+
+            HashSet<String> stringHashtags = new HashSet<>();
+            // Finding hashtags using regular expressions in string and add it to HashSet
+            while (m.find()) {
+                stringHashtags.add(m.group());
+            }
+            // Putting hashtag in HashMap and increment it count
+            for (String hashtag : stringHashtags) {
+                if (hashtagsCounter.containsKey(hashtag)) {
+                    hashtagsCounter.put(hashtag, hashtagsCounter.get(hashtag) + 1);
+                } else {
+                    hashtagsCounter.put(hashtag, 1);
+                }
+            }
+        }
+
+        // Sorting by repeatability
+        List<Map.Entry<String, Integer>> sortedHashtagTop = hashtagsCounter.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).toList();
+
+        List<Map.Entry<String, Integer>> topFiveHashtags = new LinkedList<>();
+
+        // The return is no more than the top 5
+        for (int i = 0; i < sortedHashtagTop.size() && i < 5; i++) {
+            topFiveHashtags.add(sortedHashtagTop.get(i));
+        }
+        return topFiveHashtags;
+    }
 
     public static void main(String[] args) {
 
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,7 +8,7 @@ class MainTest {
 
     @Test
     void arrSort() {
-        ArrayList<Integer> testArr = new ArrayList<Integer>();
+        ArrayList<Integer> testArr = new ArrayList<>();
         // -5 -4 -3 -2 -1 0 1 2 3 4 5
         for (int i = -5; i <= 5; i++) {
             testArr.add(i);
@@ -16,11 +16,46 @@ class MainTest {
 
         ArrayList<Integer> actual = Main.arrSort(testArr);
 
-        ArrayList<Integer> expected = new ArrayList<Integer>();
+        ArrayList<Integer> expected = new ArrayList<>();
         // 5 4 3 2 1 0
         for (int i = 5; i >= 0; i--) {
             expected.add(i);
         }
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void topFiveHashtags() {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("#Lorem #Lorem #Lorem ipsum dolor sit #amet, consectetur adipiscing #elit, sed do eiusmod #tempor incididunt ut #labore et dolore magna aliqua.");
+        strings.add("#Lorem #Lorem ipsum dolor sit #amet, consectetur adipiscing #elit #elit, sed do eiusmod #tempor incididunt ut labore et dolore magna aliqua.");
+        strings.add("#Lorem ipsum dolor sit #amet, consectetur adipiscing elit, sed do eiusmod #tempor incididunt ut labore et dolore magna aliqua.");
+        strings.add("#Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod #tempor #tempor incididunt ut labore et dolore magna aliqua.");
+        strings.add("#Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod #tempor #tempor incididunt ut labore et dolore magna aliqua.");
+
+        List<Map.Entry<String, Integer>> actual;
+        actual = Main.topFiveHashtags(strings);
+
+        // Size checking
+        if (actual.size() > 5) fail("Array size more than 5: " + actual.size());
+
+        // Hashtag checking
+        for (Map.Entry<String, Integer> entry : actual) {
+            if (entry.getKey().toCharArray()[0] != '#') {
+                fail("Not hashtag in top\n" + actual);
+            }
+        }
+
+        // Checking for correct sorting
+        Iterator<Map.Entry<String, Integer>> entryIterator = actual.iterator();
+        int value = entryIterator.next().getValue();
+        while (entryIterator.hasNext()) {
+            int nextValue = entryIterator.next().getValue();
+            if (nextValue > value) {
+                fail("Incorrect sorting\n" + actual);
+            }
+        }
+
     }
 }
